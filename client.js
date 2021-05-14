613980358c2d8820c23715db21aaee4e114c2018
ernest <ernest_everstov@mail.ru>
2021-05-14 13:42:36 +0300

    added photos made main menu added additional data

--- client.js
+++ client.js
@@ -1,15 +1,117 @@
-import { ReactInstance } from 'react-360-web';
+import { ReactInstance, Module, Surface } from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
-  const r360 = new ReactInstance(bundle, parent, {
+  r360 = new ReactInstance(bundle, parent, {
     fullScreen: true,
+    nativeModules: [
+      new surfaceModule()
+    ],
     ...options,
   });
-  r360.renderToSurface(
-    r360.createRoot('hello_vr', {}),
-    r360.getDefaultSurface()
+
+  introPanel = new Surface(
+    1000,
+    600,
+    Surface.SurfaceShape.Cylinder
   );
-  r360.compositor.setBackground(r360.getAssetURL('2.jpg'));
+
+
+
+  introRoot = r360.renderToSurface(
+    r360.createRoot('hello_vr', {}),
+    introPanel
+  );
+
+  PhotoInfoFirst = new Surface(
+    300,
+    300,
+    Surface.SurfaceShape.Flat
+  );
+
+  PhotoInfoFirst.setAngle(
+    0.2,
+    0
+  )
+
+  PhotoInfoSecond = new Surface(
+    300,
+    300,
+    Surface.SurfaceShape.Flat
+  );
+
+  PhotoInfoSecond.setAngle(
+    -Math.PI / 2,
+    0
+  )
+
+  PhotoInfoTree = new Surface(
+    300,
+    300,
+    Surface.SurfaceShape.Flat
+  );
+
+  PhotoInfoTree.setAngle(
+    2,
+    0
+  )
+
+  PhotoInfoFo = new Surface(
+    300,
+    300,
+    Surface.SurfaceShape.Flat
+  );
+
+  PhotoInfoFo.setAngle(
+    3.6,
+    0
+  )
+
+  r360.compositor.setBackground(r360.getAssetURL('21.jpg'));
+}
+
+class surfaceModule extends Module {
+  constructor() {
+    super('surfaceModule');
+  }
+
+  resizeSurface(width, hight, id) {
+    switch (id) {
+      case id == 1:
+        PhotoInfoFirst.resize(width, hight);
+        break;
+      case id == 2:
+        PhotoInfoSecond.resize(width, hight);
+        break;
+      case id == 3:
+        PhotoInfoTree.resize(width, hight);
+        break;
+      case id == 4:
+        PhotoInfoFo.resize(width, hight);
+        break;
+
+    }
+  }
+
+  start() {
+    r360.renderToSurface(
+      r360.createRoot('InfoPanel', { id: 1, text: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor' }),
+      PhotoInfoFirst
+    );
+    r360.renderToSurface(
+      r360.createRoot('InfoPanel', { id: 2, text: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor' }),
+      PhotoInfoSecond
+    );
+    r360.renderToSurface(
+      r360.createRoot('InfoPanel', { id: 3, text: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor' }),
+      PhotoInfoTree
+    );
+    r360.renderToSurface(
+      r360.createRoot('InfoPanel', { id: 4, text: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor' }),
+      PhotoInfoFo
+    );
+
+    r360.detachRoot(introRoot)
+  }
 }
 
 window.React360 = { init };
--- index.js
+++ index.js
@@ -1,53 +1,152 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View,
-  Image
+  Image,
+  VrButton,
+  NativeModules,
+  asset
 } from 'react-360';
 
-const hello_vr = () => {
-  return (
-    <View style={styles.panel}>
-      <View style={styles.title_box}>
-        <Text style={styles.title}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-          </Text>
+const surfaceModule = NativeModules.surfaceModule;
+
+class InfoPanel extends React.Component {
+  state = {
+    img: {
+      name: 'Vos.png',
+      width: 200,
+      height: 100,
+      show: false
+    }
+  }
+
+  transformDisplay(id) {
+    this.changeSurface(300, 200, id);
+    this.setState({
+      img: {
+        name: `${id}.jpg`,
+        width: 300,
+        height: 200,
+        show: true
+      }
+    });
+  }
+
+  resetPanel(id) {
+    this.changeSurface(200, 100, id);
+    this.setState({
+      img: {
+        name: 'Vos.png',
+        width: 200,
+        height: 100,
+        show: false
+      }
+    })
+  }
+  changeSurface(width, hight, id) {
+    surfaceModule.resizeSurface(width, hight, id)
+  }
+  render() {
+    let { img } = this.state
+    return (
+      <View style={styles.display_panel} onEnter={() => { this.transformDisplay(this.props.id) }} onExit={() => { this.resetPanel(this.props.id) }}>
+        <Image source={asset(`${img.name}`)} style={{ height: img.height, width: img.width }} />
+        {this.state.img.show ?
+          <View style={styles.textVeiwBlock}>
+            <Text style={styles.textView}>{this.props.text}</Text>
+          </View>
+          : <Text></Text>}
       </View>
-      <View style={styles.text_box}>
-        <Text style={styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </Text>
-      </View>
-    </View>
-  );
+    )
+  }
 }
 
-export default hello_vr
+export default class hello_vr extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      object: [
+        {
+          text: 'Осмотреть',
+          change: () => { surfaceModule.start() }
+        },
+        {
+          text: 'Настройки',
+          change: '#'
+        },
+        {
+          text: 'О нас',
+          change: '#'
+        },
+        {
+          text: 'Выход',
+          change: '#'
+        }
+      ]
+    };
+  }
+  render() {
+
+    return (
+      <View style={styles.panel}>
+        <View style={styles.title_box}>
+          <Text style={styles.title}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          </Text>
+        </View>
+        <View style={styles.menu_list}>
+          {this.state.object.map((item, index) => (
+            <VrButton style={styles.button_castom} key={index} onClick={() => item.change()}>
+              <Text style={styles.text}>
+                {item.text}
+              </Text>
+            </VrButton>
+          ))
+          }
+        </View>
+
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   panel: {
     width: 1000,
     height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    backgroundColor: 'rgba(0, 0, 0, 0.55)',
     display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: '5px, 5px, 5px, 5px'
   },
-  title_box: {
-
-  },
-  text_box: {
+  menu_list: {
     marginTop: 50,
   },
   text: {
-    color: 'black'
+    color: 'black',
+    fontSize: 25,
+    marginTop: 10
   },
   title: {
     color: 'black',
     fontSize: 20,
+  },
+  display_panel: {
+    flexDirection: 'column',
+  },
+  textVeiwBlock: {
+    padding: 10,
+    backgroundColor: 'rgba(0, 0, 0, 0.55)',
+    borderColor: '#C4002F',
+    borderWidth: 2,
+    width: 300
+  },
+  textView: {
+    color: 'black'
   }
 });
 
 AppRegistry.registerComponent('hello_vr', () => hello_vr);
+AppRegistry.registerComponent('InfoPanel', () => InfoPanel);
+
